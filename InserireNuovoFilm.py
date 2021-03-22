6be4d9aba8404df644e5dd19896308f4471d6e82
Lwarg <splinter95@live.it>
2021-03-22 23:29:54 +0100

    Draft funzione inserimento film

---
+++ InserireNuovoFilm.py
@@ -0,0 +1,56 @@
+import requests
+import json
+
+
+
+# L'utente inserisce il titolo del film
+film = input("Inserire il titolo del film: ")  # diventa casella di inserimento testo nella GUI
+
+
+# ricerca film tramite API
+apikey = "Insert valid api key" # 1000 requests al giorno
+url = "http://www.omdbapi.com/?s="+film+"&apikey="+apikey
+response = requests.request("GET", url)
+# print(response)
+data = json.loads(response.text)
+
+try: # se la ricerca tramite API ha funzionato senza errori
+
+    # Elenco film con il titolo inserito
+    possibili_film = []
+    count = 1
+
+    # se sono stati trovati dei film stampali a video con un codice numerico
+    if len(data['Search'])>0: 
+        for movie in data['Search']:
+            possibili_film.append(movie['Title'])
+            print(count,"-",movie['Title'],"-",movie['Year'])
+            count +=1
+
+        # L'utente seleziona il film desiderato tra quelli trovati inserendo il codice numerico corrispondente
+        codice = int(input("Inserire il codice del film desiderato: "))
+        film_scelto = possibili_film[codice-1]
+
+        # Verifica che il film non sia già presente in elenco
+        ## USO UN FILE TXT COME DATABASE FITTIZIO, FINCHÈ NON C'È IL DATABASE SERIO
+        elenco_film = open("elencoFilm.txt",'r')
+        gia_presente = False
+        for line in elenco_film:
+            if film_scelto==line[:-1]:
+                gia_presente = True
+                print("Film già presente in elenco")
+                break
+        elenco_film.close()
+
+        # Se non è già presente, il film viene aggiunto alla lista dei film 
+        if not gia_presente:
+            elenco_film = open("elencoFilm.txt",'a')
+            elenco_film.write(film_scelto+"\n")
+            elenco_film.close()
+
+            # Messaggio di output all'utente (successo oppure errore) 
+            print(film_scelto,'aggiunto correttamente')
+    else:
+        print('Film NON trovato')
+except:
+    print('API request error')
